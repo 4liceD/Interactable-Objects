ef97771d8f7f1373e67fbe702d7fe92eac7505f9
AliceD <95102992+4liceD@users.noreply.github.com>
2023-03-01 17:20:47 +0100

    ltcgi toggle

---
+++ Mirror/ltcgiToggle.cs
@@ -0,0 +1,37 @@
+﻿//Based on pimakers's example script
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class ltcgiToggle : UdonSharpBehaviour
+{
+    [SerializeField] MirrorManager manager;
+    [SerializeField] GameObject MirrorToToggle;
+    [SerializeField] BoxCollider buttonCollider;
+    public LTCGI_UdonAdapter Adapter;
+    public bool StartingState = true;
+    private bool state;
+    void Start()
+    {
+        state = StartingState;
+        Adapter._SetGlobalState(state);
+        if (!Networking.LocalPlayer.IsUserInVR())
+        {
+            buttonCollider.enabled = true;
+        }
+    }
+    public void _Desktopbutton()
+    {
+        manager.ToggleMirror(MirrorToToggle);
+        state = !state;
+        Adapter._SetGlobalState(state);
+    }
+    public void _SendMaxEvent()
+    {
+        manager.ToggleMirror(MirrorToToggle);
+        state = !state;
+        Adapter._SetGlobalState(state);
+    }
+}
